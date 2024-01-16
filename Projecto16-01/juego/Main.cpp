94a317f1b4fcbb9fa00bb21f79adc09371db4f03
Vicieta31 <125082380+Vicieta31@users.noreply.github.com>
2024-01-16 15:57:13 +0100

    proyectoTerminado

--- Projecto16-01/juego/Main.cpp
+++ Projecto16-01/juego/Main.cpp
@@ -1,9 +1,33 @@
-#include <iostream>
+#include "VideoManager.h"
+#include "ResourceManager.h"
+
 using namespace std;
 
-int main()
-{
-	std::cout << "Hola";
+int main(int argc, char* args[]) {
+
+	ResourceManager::getInstance()->loadAndGetGraphicID("TestResources/Bliss.png", 1);
+
+	int Two = ResourceManager::getInstance()->loadAndGetGraphicID("TestResources/nada.jpg", 1);
+
+	ResourceManager::getInstance()->removeGraphic("TestResources/Bliss.png");
+
+	int Tres = ResourceManager::getInstance()->loadAndGetGraphicID("TestResources/puke.jpeg", 1);
+
+	ResourceManager::getInstance()->setAlphaGraphic(Tres, 128);
+
+	ResourceManager::getInstance()->printLoadedGraphics();
+
+	VideoManager::getInstance()->renderGraphic(Two, 0, 0, 1080, 720);
+
+	VideoManager::getInstance()->updateScreen();
+
+	VideoManager::getInstance()->waitTime(1000);
+
+	VideoManager::getInstance()->renderGraphic(Tres, 0, 0, 1080, 720);
+
+	VideoManager::getInstance()->updateScreen();
+
+	VideoManager::getInstance()->waitTime(5000);
 
 	return 0;
 }
