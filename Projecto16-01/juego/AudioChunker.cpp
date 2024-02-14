0344824975281bebf2e5715d270ec6f192d802cd
XavierEpia <144822239+xavierguilepia@users.noreply.github.com>
2024-02-14 16:55:20 +0100

    Added AudioChunker function
    This function executes the command from the audiomanager that adds sound files, dont to reduce clutter in main, and for organisation.

---
+++ Projecto16-01/juego/AudioChunker.cpp
@@ -0,0 +1,17 @@
+#include "AudioChunker.h"
+#include "AudioManager.h"
+
+void AudioChunker() {
+
+	//Total Count of audio files: 0
+
+	/*
+		To add an audio file to the project, simply execute this command:
+
+		AudioManager::getInstance()->loadAndGetSoundID(PATH);
+
+		With PATH replaced by the file's path in quotes ("").
+
+	*/
+
+}
