fcda6a04523918d2ffd115ccb13e04eac8f01eda
XavierEpia <144822239+xavierguilepia@users.noreply.github.com>
2024-02-21 16:37:03 +0100

    Updated AudioManager Comments
    Better readability and tutorials for use of methods related to audio.

--- Projecto16-01/juego/AudioChunker.cpp
+++ Projecto16-01/juego/AudioChunker.cpp
@@ -3,15 +3,28 @@
 
 void AudioChunker() {
 
-	//Total Count of audio files: 0
-
 	/*
 		To add an audio file to the project, simply execute this command:
 
 		AudioManager::getInstance()->loadAndGetSoundID(PATH);
 
-		With PATH replaced by the file's path in quotes ("").
+		With PATH replaced by the file's path in quotes("") in this function, then call this function in the main.
 
 	*/
+
+	//Total Count of audio files: 8
+
+	//Placeholder Audio Files: 8
+
+	AudioManager::getInstance()->loadAndGetSoundID("Sounds/death.wav");
+	AudioManager::getInstance()->loadAndGetSoundID("Sounds/gameover.wav");
+	AudioManager::getInstance()->loadAndGetSoundID("Sounds/Monster-Snoring.wav");
+	AudioManager::getInstance()->loadAndGetSoundID("Sounds/shock-wall.wav");
+	AudioManager::getInstance()->loadAndGetSoundID("Sounds/step-human.wav");
+	AudioManager::getInstance()->loadAndGetSoundID("Sounds/step-monster.wav");
+	AudioManager::getInstance()->loadAndGetSoundID("Sounds/victory.wav");
+	AudioManager::getInstance()->loadAndGetSoundID("Sounds/waterfall.wav");
+
+	//Final Version Audio Files: 0
 
 }
